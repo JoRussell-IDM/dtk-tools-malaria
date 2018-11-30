3fdb8757015909903cb353e026e9a4b85148696b
jorussell <jorussell@idmod.org>
2018-11-30 15:25:10 -0800

    Added more informative header

--- examples/malariatherapy/run_malariatherapy_dtk.py
+++ examples/malariatherapy/run_malariatherapy_dtk.py
@@ -1,21 +1,36 @@
+
+#title: run_malariatherapy_dtk.py
+#
+#description: An example script for running malariatherapy challenge bite style infections where infection shapes
+# are drawn using scalable transitions as decsribed in the Malaria 2.0 work. 
+#
+#author: Jon Russell
+#
+#date: 11/29/2018
+#
+#notes and dependencies: Uses a special config 'from-cfg.json' in the bin directory to use the updates immune model
+#
+#Institute for Disease Modeling, Bellevue, WA
+
+
+
 import os
 from dtk.utils.core.DTKConfigBuilder import DTKConfigBuilder
 from dtk.generic.climate import set_climate_constant
-
 from simtools.SetupParser import SetupParser
 from simtools.ExperimentManager.ExperimentManagerFactory import ExperimentManagerFactory
 from malaria.interventions.malaria_challenge import add_challenge_trial
 from malaria.reports.MalariaReport import add_patient_report
 from simtools.ModBuilder import  ModFn, ModBuilder
 from immunity_transitions_configuration import set_transition_matrix
+
+
 # General
+working_dir =  r'C:/IDM/dtk-tools-malaria/examples/malariatherapy/'
 exp_name = 'Malariatherapy_2pt0_infections'
 years = 1  # length of simulation, in years
 immunity_forcing_on = 1
-
-#Reading in from a config file specific for Malaria Functional Model which relies on 2.0 logic
-##TODO Change to include user agnostic input folder address
-config_path = r'C:\IDM\dtk-tools-malaria\examples\malariatherapy\input\from-cfg.json'
+config_path = os.path.join(working_dir, 'input/from-cfg.json')
 
 # Setup ----------------------------------------------------------------------------------------------------------
 cb = DTKConfigBuilder.from_files(config_path)
@@ -23,21 +38,18 @@ cb.update_params({'Vector_Species_Names' : [],
                   'Simulation_Duration' : 365*years,
                   'Demographics_Filenames' : ['Malariatherapy_demographics.json']
                   })
-
 set_climate_constant(cb)
 
 # Specify immune parameters --------------------------------------------------------------------------------------
 if immunity_forcing_on == 1:
 
-    Transition_matrix = cb.config['parameters']['Parasite_Peak_Density_Probabilities']
-    #What rows am I going to go about scaling? (Reminder... Row 0 transitions from truezero, and Row 6 is from highest density)
-    immune_stim_threshold = 1
-    #whats the base parameter I want to use to describe how immunity impacts these different density classes?
-    #whats the base parameter I want to use to describe how immunity impacts the transitions within a density class?
+    #Pull the naive transitin matrix values from the config
+    transition_matrix = cb.config['parameters']['Parasite_Peak_Density_Probabilities']
+    #Sepcify the scaling of the transitio matrix to be applied (representing increasing immune pressure)
     scale_factor_array = [2,5,10,100]
 
     builder = ModBuilder.from_combos(
-        [ModFn(set_transition_matrix, Transition_matrix, scale_factor) for scale_factor in scale_factor_array]
+        [ModFn(set_transition_matrix, transition_matrix, scale_factor) for scale_factor in scale_factor_array]
     )
 else:
     builder = ''
